7af0af6629bc31b4a391bcc1d90cd5e008372961
Varun Malaviya <varunmalaviya7@gmail.com>
2023-05-24 17:36:55 -0400

    added passport local strategy to login verified users to our app. I had express-sessions installed instead of express-session which caused the api to fail. Uninstalled the wrong one and installed the correct dependency. server is back to functioning normally.

--- server/app.js
+++ server/app.js
@@ -2,23 +2,24 @@ const express = require('express')
 const cors = require('cors')
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
-const session = require('express-sessions')
+const session = require('express-session')
 const bcrypt = require('bcryptjs')
 const mongoose = require('mongoose')
 require('dotenv').config()
-
-//User model
-const User = require('./models/Users')
-
-const app = express()
 
 //Database configuration
 const db_string = process.env.MONGODB_CONNECTION_STRING
 
 //connecting to database
-mongoose.connect(db_string, { useNewUrlParser: true })
+mongoose.connect(db_string, { useUnifiedTopology: true, useNewUrlParser: true })
 .then(() => console.log('Connection to database established!'))
 .catch(err => console.log(err))    
+
+//User model
+const User = require('./models/Users')
+
+//initializing express
+const app = express()
 
 //setting up passport local strategy
 passport.use(
@@ -28,7 +29,7 @@ passport.use(
       if (!user) {
         return done(null, false, { message: "Incorrect username" });
       };
-        brcrypt.compare(password, user.password, (err, res) => {
+        bcrypt.compare(password, user.password, (err, res) => {
         if (res) {
             // passwords match! log user in
             return done(null, user)
@@ -59,11 +60,20 @@ passport.deserializeUser(async function(id, done) {
 app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
+//creating local variables using middleware
+app.use(function(req, res, next) {
+  res.locals.currentUser = req.user;
+  next();
+});
 
 //setting up CORS so client can send requests to the server. Client is hosted at port 5173 for dev.
 app.use(cors())
 
 app.use(express.json())
+
+//route for letting users register their accounts.
 app.post('/register', (req,res) => {
     User.findOne({ username: req.body.username })
         .then(user => {
@@ -102,7 +112,20 @@ app.post('/register', (req,res) => {
 
 //route for letting users login
 app.post('/login', (req,res) => {
+    passport.authenticate("local", {
+        successRedirect: "/",
+        failureRedirect: "/"
+    })
+})
 
+//route for logging out
+app.get("/log-out", (req, res, next) => {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
 })
 
 const port = process.env.PORT || 5000
